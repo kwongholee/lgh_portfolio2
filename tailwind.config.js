0e6838b0d99b6a9b2f5428cbf30b4c6ac259ac6d
kwongholee <ghlee9883@gmail.com>
2024-02-02 18:51:04 +0900

    24.02.02 chore - tailwindcss setting 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*/.{js,jsx,ts,tsx}",
-    "./components/**/*/.{js,jsx,ts,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}", 
+    "./pages/**/*.{js,ts,jsx,tsx}", 
+    "./components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {},
