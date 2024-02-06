a3cce05746db6f7b3cd3d93fb96a6017c979ed46
kwongholee <ghlee9883@gmail.com>
2024-02-06 11:16:22 +0900

    24.02.06 feat - work page data 추가

--- app/_utils/work/data.js
+++ app/_utils/work/data.js
@@ -1,7 +1,17 @@
+import onlinestudyplatform from '../../../public/image/work/onlinestudyplatform.png';
 import scrumsprint from '../../../public/image/work/scrumsprint.png';
 import ghblog from '../../../public/image/work/ghblog.png';
 
 const data = [
+  {
+    image: onlinestudyplatform,
+    text: {
+      pagename: "Online Study Platform",
+      pageInstruction: "학술 동아리 리턴에서 제작한 온라인 스터디 플랫폼 페이지",
+      techStack: "Node.js, MongoDB, Mongoose",
+      githubLink: "https://github.com/KHU-RETURN/Online-Study-Platform"
+    }
+  },
   {
     image: scrumsprint,
     text: {
