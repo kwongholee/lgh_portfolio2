4cb92e46e66a49fcaed8b38e6ea658a59449e411
kwongholee <ghlee9883@gmail.com>
2024-02-11 17:02:49 +0900

    24.02.11 feat - question instruction page timeline component add

--- app/(routes)/question/instruction/page.js
+++ app/(routes)/question/instruction/page.js
@@ -1,9 +1,12 @@
+import ITimeline from "@/app/_components/(question)/instruction/timeline/ITimeline";
 import Textbox from "@/app/_components/Textbox";
 
 export default function Page() {
   return(
     <div>
       <Textbox english={"Before ask to me..."} korean={"질문 하기 전 주의사항!"} />
+      
+      <ITimeline />
     </div>
   )
 }
