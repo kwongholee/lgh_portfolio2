f0bba1914f3d021fccd861661ff691055890a950
kwongholee <ghlee9883@gmail.com>
2024-03-21 22:17:51 +0900

    24.03.21 feat - introduce page content update

--- app/(routes)/introduce/page.js
+++ app/(routes)/introduce/page.js
@@ -1,9 +1,14 @@
 import TimelineComponent from "@/app/_components/(introduce)/timeline/TimelineComponent";
+import Textbox from "@/app/_components/Textbox";
 
 export default function Introduce() {
   return(
     <div>
-      <TimelineComponent />
+      <Textbox english={"The Story of GwanghoLee..."} korean={"이광호 일대기 (계속 채워질 예정)"} />
+
+      <div style={{width: "70%", marginLeft: "15%"}}>
+        <TimelineComponent />
+      </div>
     </div>
   )
 }
--- app/_components/(introduce)/timeline/TimelineComponent.js
+++ app/_components/(introduce)/timeline/TimelineComponent.js
@@ -20,7 +20,7 @@ export default function TimelineComponent() {
           <TimelineDot />
           <TimelineConnector />
         </TimelineSeparator>
-        <TimelineContent>Eat</TimelineContent>
+        <TimelineContent>2003.05.07 <strong>"이광호 탄생"</strong></TimelineContent>
       </TimelineItem>
 
       <TimelineItem>
@@ -28,7 +28,7 @@ export default function TimelineComponent() {
           <TimelineDot />
           <TimelineConnector />
         </TimelineSeparator>
-        <TimelineContent>Eat</TimelineContent>
+        <TimelineContent>2010 <strong>"성동초 입학"</strong></TimelineContent>
       </TimelineItem>
 
       <TimelineItem>
@@ -36,14 +36,38 @@ export default function TimelineComponent() {
           <TimelineDot />
           <TimelineConnector />
         </TimelineSeparator>
-        <TimelineContent>Eat</TimelineContent>
+        <TimelineContent>2016 <strong>"경신중 입학"</strong></TimelineContent>
+      </TimelineItem>
+      
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineDot />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>2019 <strong>"대구고 입학"</strong></TimelineContent>
+      </TimelineItem>
+
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineDot />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>2022 <strong>"경희대 입학"</strong></TimelineContent>
+      </TimelineItem>
+
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineDot />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>2023 <strong>"경희대 컴퓨터공학부 부학생회장"</strong></TimelineContent>
       </TimelineItem>
 
       <TimelineItem>
         <TimelineSeparator>
           <TimelineDot />
         </TimelineSeparator>
-        <TimelineContent>Code</TimelineContent>
+        <TimelineContent>??? <span style={{color: "red", fontSize: "32px"}}>경</span> <strong>"군입대"</strong> <span style={{color: "red", fontSize: "32px"}}>축</span></TimelineContent>
       </TimelineItem>
     </Timeline>
   );
